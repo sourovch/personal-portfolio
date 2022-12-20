2ec0c9e06fd81fbb0a08813a57cf05c52f2c333d
sourov <sourovch@gmail.com>
2022-12-20 18:06:52 +0600

    optimized home

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,43 +1,29 @@
-import React, { useEffect, useRef } from 'react';
 import '../../styles/home.css';
 
 function Home() {
-  const dynTextRef = useRef();
+  let index = 0;
 
-  const changeText = (workList, elm) => {
-    let index = 0;
+  const workList = [
+    'Web Developer',
+    'React Developer',
+    'Web Designer',
+  ];
 
-    const animationEndHandler = () => {
-      index === workList.length - 1 ? (index = 0) : index++;
+  const animationEndHandler = (e) => {
+    index === workList.length - 1 ? (index = 0) : index++;
 
-      elm.innerHTML = `<div class='home-dynamic-text'>${workList[index]}</div>`;
-    };
-
-    const textChanger = () => {
-      if (elm) {
-        elm.innerHTML = `<div class='home-dynamic-text'>${workList[index]}</div>`;
-
-        elm.addEventListener('animationend', animationEndHandler);
-      }
-    };
-
-    textChanger();
+    console.log(e.target);
+    e.target.parentNode.innerHTML = `<div class='home-dynamic-text'>${workList[index]}</div>`;
   };
-
-  useEffect(() => {
-    const workList = [
-      'Web Developer',
-      'React Developer',
-      'Web Designer',
-    ];
-    changeText(workList, dynTextRef.current);
-  }, []);
 
   return (
     <div className="home-container page-fade">
       <h1 className="home-main-text">Sourov Chowdhury</h1>
-      <div ref={dynTextRef} className="home-sec-text">
-        {/*dynamic text */}
+      <div
+        className="home-sec-text"
+        onAnimationEnd={animationEndHandler}
+      >
+        <div className="home-dynamic-text">{workList[index]}</div>
       </div>
     </div>
   );
