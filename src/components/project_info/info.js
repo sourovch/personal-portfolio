d04967540a42f4a4d9b2c2e58b0ddf6e448b5a08
sourov <sourovch@gmail.com>
2023-02-13 11:20:46 +0600

    tweeded embed

--- src/components/project_info/info.js
+++ src/components/project_info/info.js
@@ -40,7 +40,7 @@ function ProjectInfo() {
           {src && type !== 'vid' && <img src={src} alt="featured" />}
           {type === 'vid' && embedId && (
             <iframe
-              src={`https://www.youtube.com/embed/${embedId}?autoplay=1&mute=1`}
+              src={`https://www.youtube.com/embed/${embedId}?autoplay=1&mute=1&rel=0`}
               title="YouTube video player"
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
